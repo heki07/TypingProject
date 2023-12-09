c6339a596387b0b9536659edbfc5eab8b47ae03e
heki07 <153263916+heki07@users.noreply.github.com>
2023-12-09 12:05:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ const paragraphs = [
     "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless toy. A board is the cast of a religion. In ancient times the first stinko sailboat is, in its own way, an exchange. Few can name a tutti channel that isn't a footless operation. Extending this logic, an oatmeal is the rooster of a shake. Those step-sons are nothing more than matches.",
 ];
 
-const typingText = document.querySelector(".typing-text p")
-const inpField = document.querySelector(".wrapper .input-field")
-const tryAgainBtn = document.querySelector(".content button")
-const timeTag = document.querySelector(".time span b")
-const mistakeTag = document.querySelector(".mistake span")
-const wpmTag = document.querySelector(".wpm span")
-const cpmTag = document.querySelector(".cpm span")
-const accuracyTag = document.querySelector(".accuracy span")
+const typingText = document.querySelector(".typing-text p");
+const inpField = document.querySelector(".wrapper .input-field");
+const tryAgainBtn = document.querySelector(".content button");
+const timeTag = document.querySelector(".time span b");
+const mistakeTag = document.querySelector(".mistake span");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
+const accuracyTag = document.querySelector(".accuracy span");
 
 let timer;
 let maxTime = 60;
@@ -35,100 +35,102 @@ let timeLeft = maxTime;
 let charIndex = mistakes = isTyping = 0;
 
 function loadParagraph() {
-    const ranIndex = Math.floor(Math.random() * paragraphs.length);
-    typingText.innerHTML = "";
-    paragraphs[ranIndex].split("").forEach(char => {
-        console.log(char);
-        let span = `<span>${char}</span>`
-        typingText.innerHTML += span;
-    });
-    typingText.querySelectorAll("span")[0].classList.add("active");
-    document.addEventListener("keydown", () => inpField.focus());
-    typingText.addEventListener("click", () => inpField.focus());
+  const ranIndex = Math.floor(Math.random() * paragraphs.length);
+  typingText.innerHTML = "";
+  paragraphs[ranIndex].split("").forEach(char => {
+    let span = `<span>${char}</span>`;
+    typingText.innerHTML += span;
+  });
+  typingText.querySelectorAll("span")[0].classList.add("active");
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
 }
 
 function initTyping() {
-    let characters = typingText.querySelectorAll("span");
-    let typedChar = inpField.value.split("")[charIndex];
-    if (charIndex < characters.length - 1 && timeLeft > 0) {
-        if (!isTyping) {
-            timer = setInterval(initTimer, 1000);
-            isTyping = true;
+  let characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (charIndex < characters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000);
+      isTyping = true;
+    }
+    if (typedChar == null) {
+      if (charIndex > 0) {
+        charIndex--;
+        if (characters[charIndex].classList.contains("incorrect")) {
+          mistakes--;
         }
-        if (typedChar == null) {
-            if (charIndex > 0) {
-                charIndex--;
-                if (characters[charIndex].classList.contains("incorrect")) {
-                    mistakes--;
-                }
-                characters[charIndex].classList.remove("correct", "incorrect");
-            }
-        } else {
-            if (characters[charIndex].innerText == typedChar) {
-                characters[charIndex].classList.add("correct");
-            } else {
-                mistakes++;
-                characters[charIndex].classList.add("incorrect");
-            }
-            charIndex++;
-        }
-        characters.forEach(span => span.classList.remove("active"));
-        characters[charIndex].classList.add("active");
-
-        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0: wpm;
-
-        wpmTag.innerText = wpm;
-        mistakeTag.innerText = mistakes;
-        cpmTag.innerText = charIndex - mistakes;
+        characters[charIndex].classList.remove("correct", "incorrect");
+      }
     } else {
-        clearInterval(timer);
-        inpField.value = "";
+      if (characters[charIndex].innerText == typedChar) {
+        characters[charIndex].classList.add("correct");
+      } else {
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
     }
+    characters.forEach(span => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+
+    let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+    wpmTag.innerText = wpm;
+    mistakeTag.innerText = mistakes;
+    cpmTag.innerText = charIndex - mistakes;
+  } else {
+    clearInterval(timer);
+    inpField.value = "";
+    displayAccuracy();
+  }
 }
+
 function calculateAccuracy() {
-    const characters = typingText.querySelectorAll("span");
-    
-    if (characters.length === 0) {
-        return 100; // Return 100% accuracy when there are no characters
-    }
+  const characters = typingText.querySelectorAll("span");
 
-    const correctCharacters = Array.from(characters).filter(char => char.classList.contains("correct"));
-    const accuracy = (correctCharacters.length / characters.length) * 100;
-    return accuracy.toFixed(2); // Keep accuracy up to two decimal places
+  if (characters.length === 0) {
+    return 100;
+  }
+
+  const correctCharacters = Array.from(characters).filter(char => char.classList.contains("correct"));
+  const accuracy = (correctCharacters.length / characters.length) * 100;
+  return accuracy.toFixed(2);
 }
-
 
 function initTimer() {
-    if (timeLeft > 0) {
-        timeLeft--;
-        timeTag.innerText = timeLeft;
-        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-        wpmTag.innerText = wpm;
-    } else {
-        clearInterval(timer);
-        displayAccuracy(); // Call displayAccuracy when the timer reaches zero
-    }
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+    let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+    wpmTag.innerText = wpm;
+  } else {
+    clearInterval(timer);
+    displayAccuracy();
+  }
 }
 
 function displayAccuracy() {
-    const accuracy = calculateAccuracy();
-    accuracytag.innerText = accuracy + "%";
+  const accuracy = calculateAccuracy();
+  accuracyTag.innerText = accuracy + "%";
 }
 
 function resetGame() {
-    loadParagraph();
-    clearInterval(timer);
-    timeLeft = maxTime;
-    charIndex = mistakes = isTyping = 0;
-    inpField.value = "";
-    timeTag.innerText = timeLeft;
-    wpmTag.innerText = 0;
-    mistakeTag.innerText = 0;
-    cpmTag.innerText = 0;
-    displayAccuracy(); // Call displayAccuracy when the game is reset
+  loadParagraph();
+  clearInterval(timer);
+  timeLeft = maxTime;
+  charIndex = mistakes = isTyping = 0;
+  inpField.value = "";
+  timeTag.innerText = timeLeft;
+  wpmTag.innerText = 0;
+  mistakeTag.innerText = 0;
+  cpmTag.innerText = 0;
+  displayAccuracy();
 }
 
-loadParagraph();
-inpField.addEventListener("input", initTyping);
-tryAgainBtn.addEventListener("click", resetGame);
+document.addEventListener("DOMContentLoaded", function () {
+  loadParagraph();
+  inpField.addEventListener("input", initTyping);
+  tryAgainBtn.addEventListener("click", resetGame);
+});
